ea57c728bdc453b551c8f568884ceb3515e32263
Nitul Bora <144526482+Nitul2023@users.noreply.github.com>
2026-07-08 00:38:25 +0530

    Update menu-script.js

--- oranges-bbq-final/menu-script.js
+++ oranges-bbq-final/menu-script.js
@@ -37,24 +37,25 @@ const menuSections = [
     id: 'sec-soups',
     label: 'Soups & Warm Bowls',
     img: 'assets/pages/soups.jpeg',
+    price:"14/160",
     items: [
       { id: 'sp-clear', title: 'Clear Soup', variants: [
           { suffix: 'Veg', price: 140 },
           { suffix: 'Chicken', price: 160 }
         ], desc: 'Fresh veggies/Chicken cubes simmered in vegetable/chicken broth and served hot.' },
-      { id: 'sp-sweetcorn', title: 'Sweet Corn Soup (Veg)', variants: [
+  { id: 'sp-sweetcorn', title: 'Sweet Corn Soup (Veg)',price:"14/160", variants: [
           { suffix: 'Veg', price: 140 },
           { suffix: 'Chicken', price: 160 }
         ], desc: 'Creamy light corn blend with assorted vegetables/chicken.' },
-      { id: 'sp-hot&sour', title:'Hot & Sour Soup', variants: [
+  { id: 'sp-hot&sour', title:'Hot & Sour Soup',price:"14/160", variants: [
           { suffix: 'Veg', price: 140 },
           { suffix: 'Chicken', price: 160 }
         ], desc: 'Mixed vegetable/chicken in a spicy soya-based soup.' },
-      { id: 'sp-Manchow', title: 'Manchow Soup', variants: [
+  { id: 'sp-Manchow', title: 'Manchow Soup',price:"14/160", variants: [
           { suffix: 'Veg', price: 140 },
           { suffix: 'Chicken', price: 160 }
         ], desc: 'Thick vegetable/chicken soup served along crispy fried noodles.' },
-      { id: 'sp-Tomato/Mushroom/Vegetable', title: 'Cream of Tomato/Mushroom/Vegetable', variants: [
+  { id: 'sp-Tomato/Mushroom/Vegetable', title: 'Cream of Tomato/Mushroom/Vegetable',price:"14/160", variants: [
           { suffix: 'Veg', price: 140 },
           { suffix: 'Chicken', price: 160 }
         ], desc: 'Thick creamy soup served with your choice.' }

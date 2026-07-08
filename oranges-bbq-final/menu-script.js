28563f999058c3bdec513faf23b1f9f42e859e5d
Nitul Bora <144526482+Nitul2023@users.noreply.github.com>
2026-07-08 12:06:57 +0530

    Update menu-script.js

--- oranges-bbq-final/menu-script.js
+++ oranges-bbq-final/menu-script.js
@@ -414,7 +414,7 @@ const menuSections = [
  {
   id: "sec-club-sandwich",
   label: "Club Sandwich Combo",
-  img: "assets/pages/combo.jpeg",
+  img: "assets/pages/club-sandwich.jpeg",
 
   categories: [
 
@@ -513,7 +513,7 @@ const menuSections = [
 {
   id: "sec-orange-combos",
   label: "Orange Barbeque Combos",
-  img: "assets/pages/combos.jpeg",
+  img: "assets/pages/orange-combos.jpeg",
 
   categories: [
 
